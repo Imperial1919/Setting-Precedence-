289f38f48cdb882b420ed254b2ec8d5a4d07d53e
Bryce <ImperialEagle1998@gmail.com>
2020-09-26 20:59:23 -0500

    Verison I

---
+++ PrecedencePractice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrecedencePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            sum = 10 - 2 * 5;
+            Console.Write("Default Order: " + sum + "\n");
+
+            sum = (10 - 2) * 5;
+            Console.Write("Forced Order:" + sum + "\n");
+
+            sum = 100 * 5 / 6 + 50;
+            Console.WriteLine("Random Example of Default Ordering: " + sum);
+
+            sum = 100 * 5 / (6 + 50);
+            Console.WriteLine("Random Example of Forced Ordering:" + sum);
+
+
+            //the compiler goes from left to right, or simply ------------>
+            sum = 7 - 4 + 2;
+            Console.WriteLine("Example of Default Direction: " + sum);
+
+
+            // The compiler goes from right to left, or simply <----------
+            sum = 7 - (4 + 2);
+            Console.WriteLine("Example of Forced Direction: " + sum);
+        }
+    }
+}
